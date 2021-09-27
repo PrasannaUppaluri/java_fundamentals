d4b3b5e8204fa6103de3d9bfa7ac6b8a35db46ca
Uppaluri Ganagadhar Vidya <Prasanna.Uppaluri@sabre.com>
2021-09-27 12:41:42 -0500

    Firt Excercise

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -14,6 +14,23 @@ public class Exercise_04 {
 
         int i = 1;
         System.out.println("int i is: " + i);
+        int age = 38;
+        float b = 38.98f;
+        byte v = 34;
+        char c = 'A';
+        double d = 1.23456;
+        long value = 4565766788L;
+        boolean a = true;
+        short s = 567;
+
+        System.out.println("age="+ i);
+        System.out.println("float b=" +b);
+        System.out.println("byte v=" +v);
+        System.out.println("char c=" +c);
+        System.out.println("double d=" +d);
+        System.out.println("long value=" +value);
+        System.out.println("boolean a=" +a);
+        System.out.println("short s=" +s);
 
         // write your code below
 
--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -14,14 +14,20 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
-
+        int value = str.length();
+        System.out.println(value);
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
-
+        boolean b = str.equals(str2);
+        System.out.println(b);
         // please concatenate str & str2 and set the result to a new String variable below
 
+        String s = str + str2;
+        System.out.println("value of s is: " +s);
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+        System.out.println(s.indexOf("!"));
+        System.out.println(s.replace('!','+'));
 
     }
 
